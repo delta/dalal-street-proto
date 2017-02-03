9a869017f58b12e09adbc3d34e5bf02d34f722f1
Parth Thakkar <thakkarparth007@gmail.com>
2017-02-03 15:45:56 +0530

    Fix travis-test.sh to fail if any subcommand fails
    The travis-test.sh script ran successfully (return status 0) even
    if one of the protoc commands failed, that's because each ran as
    an individual command. Fixed that by adding '&&' between each
    command.

--- travis-test.sh
+++ travis-test.sh
@@ -7,18 +7,16 @@
 # files do not have compile-time errors, and the output
 # is buildable in Go.
 
-# Do not run this manually.
+# You may run this manually to test before pushing.
 
-mkdir -p proto_build/
-cp -r actions/ datastreams/ errors/ models/ DalalMessage.proto proto_build/
-cd proto_build/
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi:. *.proto
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/actions:. actions/*.proto
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/models:. models/*.proto
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/errors:. errors/*.proto
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/datastreams:. datastreams/*.proto
-grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i 's|github.com/thakkarparth007/dalal-street-proto/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g'
-find -type f -name "*.proto" -exec rm {} \;
+mkdir -p proto_build/ &&
+cp -r actions/ datastreams/ errors/ models/ DalalMessage.proto proto_build/ &&
+cd proto_build/ &&
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi:. *.proto &&
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/actions:. actions/*.proto &&
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/models:. models/*.proto &&
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/errors:. errors/*.proto &&
+protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/datastreams:. datastreams/*.proto &&
+grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i 's|github.com/thakkarparth007/dalal-street-proto/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g' &&
+find -type f -name "*.proto" -exec rm {} \; &&
 go build
-
-
