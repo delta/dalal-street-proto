ced7c2143a6150a44378dec9dfdc3f42905e9ee2
Parth Thakkar <thakkarparth007@gmail.com>
2017-12-18 14:44:21 +0530

    Simplify protobuf messages
    Having a lot of oneof fields was complicating Server code.
    This change simplifies it to a good extent.

--- travis-test.sh
+++ travis-test.sh
@@ -9,13 +9,13 @@
 
 # You may run this manually to test before pushing.
 
+rm -rf proto_build/
 mkdir -p proto_build/ &&
-cp -r actions/ datastreams/ errors/ models/ DalalMessage.proto proto_build/ &&
+cp -r actions/ datastreams/ models/ DalalMessage.proto proto_build/ &&
 cd proto_build/ &&
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi:. *.proto &&
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/actions:. actions/*.proto &&
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/models:. models/*.proto &&
-protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/errors:. errors/*.proto &&
 protoc --go_out=import_prefix=github.com/thakkarparth007/dalal-street-proto/proto_build/,import_path=socketapi/datastreams:. datastreams/*.proto &&
 grep -rl "github.com/golang/protobuf/proto" . | grep -v ".sh" | xargs sed -i 's|github.com/thakkarparth007/dalal-street-proto/proto_build/github.com/golang/protobuf/proto|github.com/golang/protobuf/proto|g' &&
 find -type f -name "*.proto" -exec rm {} \; &&
